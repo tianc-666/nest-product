a4d6c1d58ffae0ac308f670e6910e8f0a9cdd358
汪杰 <p-wangjie126@xiaomi.com>
2026-06-16 10:43:55 +0800

    fix: presigned URL use request host instead of internal Docker address

--- chat-room-backend/src/minio/minio.controller.ts
+++ chat-room-backend/src/minio/minio.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get, Inject, Query } from '@nestjs/common';
+import { Controller, Get, Inject, Query, Req } from '@nestjs/common';
+import { Request } from 'express';
 import * as Minio from 'minio';
 
 @Controller('minio')
 export class MinioController {
   @Inject('MINIO_CLIENT')
   private minioClient: Minio.Client;
+
   @Get('presignedUrl')
-  presignedPutObject(@Query('fileName') fileName: string) {
-    return this.minioClient.presignedPutObject('chat-room', fileName);
+  async presignedPutObject(@Query('fileName') fileName: string, @Req() req: Request) {
+    const url = await this.minioClient.presignedPutObject('chat-room', fileName);
+    // 用请求的 host 替换 Docker 内部地址
+    const protocol = req.headers['x-forwarded-proto'] || 'http';
+    const host = req.headers['x-forwarded-host'] || req.headers.host;
+    return url.replace(/http:\/\/minio:9000/, `${protocol}://${host}`);
   }
 }
